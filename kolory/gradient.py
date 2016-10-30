b1f4c45349369dff040aa651d624e68f95627051
lewinms <lewin.michal@gmail.com>
2016-10-30 23:06:14 +0100

    Dodano kolory hsv

--- kolory/gradient.py
+++ kolory/gradient.py
@@ -11,13 +11,18 @@ import math as m
 from matplotlib import colors
 
 samples = 1024
+rgbBw = [[0,0,0],[1,1,1]]
+rgbGbr = [[0,1,0],[0,0,1],[1,0,0]]
+rgbGbrFull = [[0,1,0],[0,1,1],[0,0,1],[1,0,1],[1,0,0]]
 rgbWbCustom = [[1,1,1],[1,0,1],[0,0,1],[0,1,1],[0,1,0],[1,1,0],[1,0,0],[0,0,0]]
 
+hsvBw = [[0,0,0],[0,0,1]]
+hsvGBR = [[120,1,1],[180,1,1],[240,1,1],[300,1,1],[360,1,1]]
+hsvUnknown = [[120,0.5,1],[60,0.5,1],[0,0.5,1]]
+hsvCustom = [[0,1,1],[120,0.9,1],[180,0.7,1],[240,0.5,1],[300,0.3,1],[310,0,1]]
+
 def getPointInCube(listPoints,point):
-    element = m.trunc(point*(len(listPoints)-1))
-    if element == len(listPoints)-1:
-        element = len(listPoints)-2
-    skala = point*(len(listPoints)-1)-element
+    element, skala = getElement(listPoints, point)
     finalPoint = []
     for i in range(len(listPoints[element])):
         if listPoints[element+1][i]-listPoints[element][i] != 0:
@@ -25,6 +30,32 @@ def getPointInCube(listPoints,point):
         else:
             finalPoint.append(listPoints[element][i])
     return finalPoint
+
+
+def getElement(listPoints, point):
+    element = m.trunc(point * (len(listPoints) - 1))
+    if element == len(listPoints) - 1:
+        element = len(listPoints) - 2
+    skala = point * (len(listPoints) - 1) - element
+    return element, skala
+
+
+def getPointInCone(listPoints,point):
+    element,skala = getElement(listPoints,point)
+    finalPoint = []
+    for i in range(len(listPoints[element])):
+        lewyZakres = listPoints[element][i]
+        prawyZakres = listPoints[element + 1][i]
+        div = prawyZakres - lewyZakres
+        if div != 0:
+            if i == 0:
+                finalPoint.append(lewyZakres + div * skala)
+            else:
+                finalPoint.append(lewyZakres + (1 / (div)) * skala)
+        else:
+            finalPoint.append(lewyZakres)
+    return finalPoint
+
 
 def plot_color_gradients(gradients, names):
     # For pretty latex fonts (commented out, because it does not work on some machines)
@@ -43,7 +74,6 @@ def plot_color_gradients(gradients, names):
     for ax, gradient, name in zip(axes, gradients, names):
         # Create image with two lines and draw gradient on it
         img = np.zeros((2, samples, 3))
-        # print(img)
         for i, v in enumerate(np.linspace(0, 1, samples)):
             img[:, i] = gradient(v)
 
@@ -60,55 +90,51 @@ def plot_color_gradients(gradients, names):
     plt.close()
 
 def hsv2rgb(h, s, v):
-    #TODO
-    return (h, s, v)
+    vs = v*s
+    hue = h/60
+    x = vs * (1 - abs((hue % 2) -1))
+    switcher = {
+        0: [vs,x,0],
+        1: [x,vs,0],
+        2: [0,vs,x],
+        3: [0,x,vs],
+        4: [x,0,vs],
+        5: [vs,0,x]
+    }
+    rgb = switcher.get(m.trunc(hue),[0,0,0])
+    match = v-vs
+    rgb = [i+match for i in rgb]
+    return rgb
 
 def gradient_rgb_bw(v):
-    #TODO
-    return (v, v, v)
-
+   return getPointInCube(rgbBw,v)
 
 def gradient_rgb_gbr(v):
-    if v < 0.5:
-        return (0,1-v*2,v*2)
-    else:
-        v-=0.5
-        return (v*2, 0,1-v*2)
+    return getPointInCube(rgbGbr,v)
 
 def gradient_rgb_gbr_full(v):
-    if v < 0.25:
-        return (0,1,4*v)
-    elif v < 0.5:
-        v-=0.25
-        return (0,1-4*v,1)
-    elif v < 0.75:
-        v-=0.5
-        return (4*v,0,1)
-    else:
-        v-=0.75
-        return (1,0,1-4*v)
+    return getPointInCube(rgbGbrFull,v)
 
 def gradient_rgb_wb_custom(v):
     #TODO
     return getPointInCube(rgbWbCustom,v)
 
 def gradient_hsv_bw(v):
-    #TODO
-    return hsv2rgb(0, 0, 0)
-
+    hsv = getPointInCone(hsvBw,v)
+    return hsv2rgb(hsv[0], hsv[1], hsv[2])
 
 def gradient_hsv_gbr(v):
-    #TODO
-    return hsv2rgb(0, 0, 0)
+    hsv = getPointInCone(hsvGBR,v)
+    return hsv2rgb(hsv[0], hsv[1], hsv[2])
 
 def gradient_hsv_unknown(v):
-    #TODO
-    return hsv2rgb(0, 0, 0)
+    hsv = getPointInCone(hsvUnknown, v)
+    return hsv2rgb(hsv[0], hsv[1], hsv[2])
 
 
 def gradient_hsv_custom(v):
-    #TODO
-    return hsv2rgb(0, 0, 0)
+    hsv = getPointInCone(hsvCustom, v)
+    return hsv2rgb(hsv[0], hsv[1], hsv[2])
 
 
 if __name__ == '__main__':
